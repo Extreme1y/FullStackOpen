c009163c55d02fcefe8ba64b83d5ae5dac203344
Cxzy <alisher.kenzhegaliev@gmail.com>
2024-08-15 22:20:55 +0500

    throw error bug fixed

--- blog-full/backend-blog/app.js
+++ blog-full/backend-blog/app.js
@@ -1,5 +1,5 @@
-const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
@@ -7,6 +7,7 @@ const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
 
 mongoose.set('strictQuery', false)
 
@@ -23,7 +24,10 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 //app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
